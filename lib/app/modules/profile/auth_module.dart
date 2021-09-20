abe230dacce8323b231670922278b33910234493
realrgt <ergitorubildo@gmail.com>
2021-09-21 01:31:24 +0200

    lint failures clean

--- lib/app/modules/profile/auth_module.dart
+++ lib/app/modules/profile/auth_module.dart
@@ -7,6 +7,6 @@ class HomeModule extends Module {
 
   @override
   final List<ModularRoute> routes = [
-    ChildRoute(Modular.initialRoute, child: (_, args) => Scaffold()),
+    ChildRoute(Modular.initialRoute, child: (_, args) => const Scaffold()),
   ];
 }
