523f7484cd1edc3467fc55d9177bc75cbf209890
realrgt <ergitorubildo@gmail.com>
2021-09-23 00:35:53 +0200

    auth module -- DI

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'modules/home/home_module.dart';
 
 class AppModule extends Module {
   @override
-  final List<Bind> binds = [];
+  final List<Bind> binds = [
+    Bind.instance(FirebaseAuth.instance),
+  ];
 
   @override
   final List<ModularRoute> routes = [
