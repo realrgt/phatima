44c3eaaf4b9ac7f9067671dc4b8c36505e8092fc
realrgt <ergitorubildo@gmail.com>
2021-09-28 10:39:34 +0200

    profile DI

--- lib/app/modules/profile/profile_module.dart
+++ lib/app/modules/profile/profile_module.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../../core/blocs/bloc_base.dart';
+import '../../core/usecase/usecase.dart';
+import 'data/datasources/user_datasource.dart';
+import 'data/datasources/user_firestore_datasource_impl.dart';
+import 'data/repositories/user_repository_impl.dart';
+import 'domain/repositories/user_repository.dart';
+import 'domain/usecases/get_user.dart';
+import 'domain/usecases/update_user.dart';
+import 'presenter/bloc/bloc.dart';
+
 class ProfileModule extends Module {
   @override
-  final List<Bind> binds = [];
+  final List<Bind> binds = [
+    //* blocs
+    Bind.lazySingleton<BloC>((i) => ProfileBloc(getUser: i(), upateUser: i())),
+    //* usecases
+    Bind.lazySingleton<IUsecase>((i) => GetUser(userRepository: i())),
+    Bind.lazySingleton<IUsecase>((i) => UpdateUser(userRepository: i())),
+    //* repositories
+    Bind.lazySingleton<IUserRepository>(
+        (i) => UserRepositoryImpl(userDataSource: i())),
+    //* datasources
+    Bind.lazySingleton<IUserDataSource>(
+        (i) => UserFirestoreDataSourceImpl(firestore: i())),
+  ];
 
   @override
   final List<ModularRoute> routes = [
